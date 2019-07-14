5b2af0fca61ca6bc244768fa50a143a29915e376
cgmBnu <51448121+cgmBnu@users.noreply.github.com>
2019-07-14 17:02:15 -0300

    teste2

---
+++ Repository/CategoriaRepository.cs
@@ -0,0 +1,21 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public class CategoriaRepository
+    {
+        public int Inserir(Categoria categoria)
+        {
+            SqlCommand comando = Conexao.Conectar();
+            comando.CommandText = "INSERT INTO "
+        }
+        
+        
+    }
+}
