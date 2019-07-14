6bc830f17426176e1b5e3f29224e51fb32f1127f
cgmBnu <51448121+cgmBnu@users.noreply.github.com>
2019-07-14 11:39:21 -0300

    Initial commit

---
+++ Repository/EstadoRepository.cs
@@ -0,0 +1,26 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public class EstadoRepository
+    {
+        public int Inserir(Estado estado)
+        {
+            SqlCommand comando = Conexao.Conectar();
+            comando.CommandText = "INSERT INTO estados (nome, sigla) OUTPUT INSERTED VALUES (@NOME, @SIGLA)";
+            comando.Parameters.AddWithValue("@NOME", estado.Nome);
+            comando.Parameters.AddWithValue("@SIGLA", estado.Sigla);
+
+            int id = Convert.ToInt32(comando.ExecuteScalar());
+            comando.Connection.Close();
+            return id;
+
+        }
+    }
+}
---
+++ View/Controllers/EstadoController.cs
@@ -0,0 +1,27 @@
+﻿using Model;
+using Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace View.Controllers
+{
+    public class EstadoController : Controller
+    {
+        private EstadoRepository repository;
+
+        public EstadoController()
+        {
+            repository = new EstadoRepository();
+
+        }
+        // GET: Estado
+        public ActionResult Index()
+        {
+            /*List<Estado> */
+            return View();
+        }
+    }
+}
