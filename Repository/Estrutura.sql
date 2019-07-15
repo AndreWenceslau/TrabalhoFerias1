9857b9de34edf8efec54710a8a3511c46ecb450f
AndreWenceslau <andrewenceslaudeoli@gmail.com>
2019-07-15 12:35:55 -0300

    Inserir Usuarios Repository

--- Repository/Estrutura.sql
+++ Repository/Estrutura.sql
@@ -7,9 +7,7 @@ senha VARCHAR(100)
 
 CREATE TABLE estados(
 id INT PRIMARY KEY IDENTITY (1,1),
-<<<<<<< HEAD
-nome_estado VACHAR(100),
-=======
+nome_estado VARCHAR(100),
 nome VARCHAR(100),
 sigla VARCHAR (2)
 );
