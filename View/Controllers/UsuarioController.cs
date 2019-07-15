4067d1c9fcf5c00aceea7cf10994b9b94fc5b01e
AndreWenceslau <andrewenceslaudeoli@gmail.com>
2019-07-15 13:01:19 -0300

    teste

--- View/Controllers/UsuarioController.cs
+++ View/Controllers/UsuarioController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Model;
+using Repository;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +10,31 @@ namespace View.Controllers
 {
     public class UsuarioController : Controller
     {
+        private UsuarioRepository repository;
+
+        public UsuarioController()
+        {
+            repository = new UsuarioRepository();
+        }
         // GET: Usuario
         public ActionResult Index()
         {
+            List<Usuario> ususarios = repository.ObterTodos();
+            ViewBag.Usuarios = ususarios; //ViewBag.Usuarios não puxou POSSÍVEL PROBLEMA
             return View();
+        }
+        public ActionResult Cadastro()
+        {
+            return View();
+        }
+        public ActionResult Store(string nome, string login, string senha)
+        {
+            Usuario usuario = new Usuario();
+            usuario.Nome = nome;
+            usuario.Login = login;
+            usuario.Senha = senha;
+            repository.Inserir(usuario);
+            return RedirectToAction("Index");
         }
     }
 }
